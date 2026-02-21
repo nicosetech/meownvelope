84129a6bf74e0d800690ee8ef44b7aa30e5ebbb5
Jake Olsen <vfv16@txstate.edu>
2026-02-20 19:35:35 -0600

    Fixed loading the env file & fixed dev names

--- webapp/app.py
+++ webapp/app.py
@@ -6,7 +6,7 @@ from forms import validate_contact_form
 from emailservice import send_support_email
 
 #Setup Environment
-load_dotenv()
+load_dotenv('env')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
