a13dac5cd4641d4a019707095ff020f9a1b0d121
Treston Fallavollita <ioj11@txstate.edu>
2026-02-22 16:06:41 -0600

    SCRUM-103: Formatted Main for Easier Customization
    Currently we are working with the default app that comes with the initialization of flutter. I want to keep this so my groupmates and I can reference it. However, I did remove this example class from the main file itself.

--- meownvelope_mobile/test/widget_test.dart
+++ meownvelope_mobile/test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:meownvelope_mobile/main.dart';
+import 'package:meownvelope_mobile/FlutterTestingPages/test_page.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ExamplePage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
