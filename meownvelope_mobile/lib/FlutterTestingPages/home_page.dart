792c70076aeb6bd70769e39f173265732ecbcb8b
Nico San Esteban <vnv21@txstate.edu>
2026-02-26 16:57:39 -0600

    SCRUM-98: Created initial Home page

---
+++ meownvelope_mobile/lib/FlutterTestingPages/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:meownvelope_mobile/utils/hive/hive_database.dart';
+
+// --Top Row-- 
+
+// -- Top Middle Section--
+
+// -- Bottom Middle Section --
+
+// -- Bottom Row -- 
