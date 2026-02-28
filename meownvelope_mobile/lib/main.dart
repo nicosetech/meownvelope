3ffd8905be12965122836e1172b2a3b6be490f15
Nico San Esteban <vnv21@txstate.edu>
2026-02-28 16:13:00 -0600

    SCRUM-98: Edited dart main to run home page app

--- meownvelope_mobile/lib/main.dart
+++ meownvelope_mobile/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:meownvelope_mobile/FlutterTestingPages/example_page.dart';
+import 'package:meownvelope_mobile/FlutterTestingPages/home_page.dart';
 import 'package:meownvelope_mobile/utils/hive/hive_database.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -8,6 +9,7 @@ void main() async{
 
   WidgetsFlutterBinding.ensureInitialized();
   await HiveDatabase.initHiveDatabase();
-  runApp(const ExamplePage());
+
+  runApp(const MeownvelopeApp());
   
 }
