51bb74832e69904df1ef62184a483b1d64aaf287
Nico San Esteban <vnv21@txstate.edu>
2026-02-28 16:12:25 -0600

    SCRUM-98: Initial scaffold of page layout

--- meownvelope_mobile/lib/FlutterTestingPages/home_page.dart
+++ meownvelope_mobile/lib/FlutterTestingPages/home_page.dart
@@ -4,10 +4,39 @@ import 'package:hive_flutter/hive_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meownvelope_mobile/utils/hive/hive_database.dart';
 
-// --Top Row-- 
+class MeownvelopeApp extends StatelessWidget {
+  const MeownvelopeApp({super.key});
 
-// -- Top Middle Section--
-
-// -- Bottom Middle Section --
-
-// -- Bottom Row -- 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          // bar to display title and paw icon
+          appBar: AppBar(),
+          //button at bottom right for new envelope
+          floatingActionButton: FloatingActionButton(),  
+          
+          //page layout
+          body: Column(
+            children: [
+              //top section to include menu button, cash visual, and buttons
+              Column(
+                children:[
+                  // menu button
+                  Row(),
+                  // cash visual
+                  Container(), 
+                  // buttons for importing and filling
+                  Row(
+                    children: [ TextButton(), TextButton()]
+                  )
+                ]
+              ), 
+              // bottom section that shows envelope previews 
+              Container()
+            ]
+          )
+        )
+        );
+  }
+}
