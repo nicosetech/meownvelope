186757c66dffe5a289bf0dca78e5bf9458f243d4
Treston Fallavollita <ioj11@txstate.edu>
2026-03-01 19:37:50 -0600

    SCRUM-98: Changed to Homepage to Stateful
    Homepage has been changed to be a stateful widget that now updates when a new envelope is created.

--- meownvelope_mobile/lib/meownvelope_app.dart
+++ meownvelope_mobile/lib/meownvelope_app.dart
@@ -5,38 +5,46 @@ import 'package:meownvelope_mobile/DataTypes/envelope_data.dart';
 import 'package:meownvelope_mobile/envelope_creation_page.dart';
 import 'package:meownvelope_mobile/utils/hive/hive_database.dart';
 
-class MeownvelopeApp extends StatelessWidget {
+class MeownvelopeApp extends StatefulWidget {
   const MeownvelopeApp({super.key});
   static const Color darkerBlue = Color.fromARGB(255, 49, 102, 128); 
   static const Color medBlue =Color.fromARGB(255, 186, 210, 229);
   static const Color lighterBlue = Color.fromARGB(255, 206, 221, 233); 
   static const Color lightestBlue = Color.fromARGB(255, 222, 232, 239); 
   static const Color backgroundBlue = Color.fromARGB(255, 230, 237, 241); 
-  
 
+  @override
+  State<MeownvelopeApp> createState() => _MeownvelopeAppState();
+}
+
+class _MeownvelopeAppState extends State<MeownvelopeApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-            backgroundColor: backgroundBlue,
+            backgroundColor: MeownvelopeApp.backgroundBlue,
             // bar to display title and paw icon
             appBar: AppBar(
-                backgroundColor: backgroundBlue,
+                backgroundColor: MeownvelopeApp.backgroundBlue,
                 title: Text("Meownvelope", style: GoogleFonts.mochiyPopPOne(textStyle: TextStyle(fontSize: 35))),
                 leadingWidth: 90,
-                leading: Image.asset('assets/cat_paw.png', color: darkerBlue),
-                foregroundColor: darkerBlue,
+                leading: Image.asset('assets/cat_paw.png', color: MeownvelopeApp.darkerBlue),
+                foregroundColor: MeownvelopeApp.darkerBlue,
                 elevation: 0,
           ),
           //button at bottom right for new envelope
           floatingActionButton: FloatingActionButton.extended(
-            backgroundColor: medBlue, 
+            backgroundColor: MeownvelopeApp.medBlue, 
             shape: RoundedRectangleBorder( 
                 borderRadius: BorderRadius.all(Radius.circular(16))
                 ),
-                icon: Icon(Icons.add, color: darkerBlue, size: 40),
-                label:Icon(Icons.mail_outline, color: darkerBlue, size: 40),
+                icon: Icon(Icons.add, color: MeownvelopeApp.darkerBlue, size: 40),
+                label:Icon(Icons.mail_outline, color: MeownvelopeApp.darkerBlue, size: 40),
                 onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (_) => const EnvelopeCreationPage(),));
+                  Navigator.push(context, MaterialPageRoute(builder: (_) => const EnvelopeCreationPage(),))
+                           .then((value) {
+                              setState(() {});
+                            },
+                          );
                 },
           ),  
           
@@ -55,10 +63,10 @@ class MeownvelopeApp extends StatelessWidget {
                         height: 70,
                         width: 70,
                         decoration: BoxDecoration(
-                            color:medBlue, 
+                            color:MeownvelopeApp.medBlue, 
                             borderRadius: BorderRadius.only(topRight: Radius.circular(8), bottomRight: Radius.circular(8))),
                         child: IconButton(
-                            icon: Icon(Icons.keyboard_double_arrow_right, color:darkerBlue, size: 60),
+                            icon: Icon(Icons.keyboard_double_arrow_right, color:MeownvelopeApp.darkerBlue, size: 60),
                             onPressed:(){ Text('pressed');}, // temporary until creation of menu bar
                             ),
                         ),
@@ -68,7 +76,7 @@ class MeownvelopeApp extends StatelessWidget {
                   Stack(
                     alignment: AlignmentGeometry.centerStart,
                         children: [
-                            Align(alignment: Alignment.bottomCenter, child: ClipOval(child: Container(width: 180, height: 60, decoration: BoxDecoration(color:medBlue)))
+                            Align(alignment: Alignment.bottomCenter, child: ClipOval(child: Container(width: 180, height: 60, decoration: BoxDecoration(color:MeownvelopeApp.medBlue)))
                             ),
                             Align(alignment: Alignment.center, child: Image.asset('assets/leastcash.png',width:150, height:120)),
                         ] 
@@ -82,8 +90,8 @@ class MeownvelopeApp extends StatelessWidget {
                                 maximumSize: Size(180,70),
                                 minimumSize: Size(130,50),
                                 elevation: 5,
-                                shadowColor: medBlue,
-                                backgroundColor: darkerBlue,
+                                shadowColor: MeownvelopeApp.medBlue,
+                                backgroundColor: MeownvelopeApp.darkerBlue,
                                 foregroundColor: Colors.white,
                                 shape: RoundedRectangleBorder(
                                     borderRadius: BorderRadius.all(Radius.circular(8)),
@@ -97,8 +105,8 @@ class MeownvelopeApp extends StatelessWidget {
                                 maximumSize: Size(180,70),
                                 minimumSize: Size(130,50),
                                 elevation: 5,
-                                shadowColor: medBlue,
-                                backgroundColor: darkerBlue,
+                                shadowColor: MeownvelopeApp.medBlue,
+                                backgroundColor: MeownvelopeApp.darkerBlue,
                                 foregroundColor: Colors.white,
                                 shape: RoundedRectangleBorder(
                                     borderRadius: const BorderRadius.all(Radius.circular(8)),
@@ -116,7 +124,7 @@ class MeownvelopeApp extends StatelessWidget {
               Container( //background box containing envelope list
                 height: 450,
                 width: 380,
-                color: lightestBlue,
+                color: MeownvelopeApp.lightestBlue,
 
                 child: GridView.count( // container that actually holds envelopes
                   crossAxisCount: 2,
@@ -128,6 +136,7 @@ class MeownvelopeApp extends StatelessWidget {
           )
         );
   }
+
   Widget envelopeBuilder(Color userColor, String userLabel) {
     return Container(
         color: userColor,
